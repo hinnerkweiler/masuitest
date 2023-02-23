adae7fecbb6642b3d2c8c4a247e9b70cb143dbdc
Hinnerk Weiler <post@hinnerk-weiler.de>
2023-02-24 00:02:51 +0100

    pushed the unpushed ;-)

--- masuitest/MainPage.xaml.cs
+++ masuitest/MainPage.xaml.cs
@@ -7,19 +7,14 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
-	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+        var mapControl = new Mapsui.UI.Maui.MapControl();
+        mapControl.Map?.Layers.Add(Mapsui.Tiling.OpenStreetMap.CreateTileLayer());
+        Content = mapControl;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+    }
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+	
 }
 
 
